4e1dbfeb8738a5ec2fb04e10dee993f83fa8c00b
yjunya <yjunya@junya-yanagisawanoMacBook-Pro.local>
2021-03-03 16:53:21 +0900

    [CHANGE] test

--- main.go
+++ main.go
@@ -1,16 +1,36 @@
+
+// Sample run-helloworld is a minimal Cloud Run service.
 package main
 
 import (
-    "fmt"
-    "net/http"
+        "fmt"
+        "log"
+        "net/http"
+        "os"
 )
 
 func main() {
-    // ①
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, Build Trigger! Test error")
-    })
+        log.Print("starting server...")
+        http.HandleFunc("/", handler)
 
-    // ②
-    http.ListenAndServe(":3000", nil)
+        // Determine port for HTTP service.
+        port := os.Getenv("PORT")
+        if port == "" {
+                port = "8080"
+                log.Printf("defaulting to port %s", port)
+        }
+
+        // Start HTTP server.
+        log.Printf("listening on port %s", port)
+        if err := http.ListenAndServe(":"+port, nil); err != nil {
+                log.Fatal(err)
+        }
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+        name := os.Getenv("NAME")
+        if name == "" {
+                name = "World"
+        }
+        fmt.Fprintf(w, "Hello %s!\n", name)
 }
